32cc7bfd959d8ffb13c72e6cabee5004138d94aa
chuyii <contact@chuyii.net>
2017-02-10 12:44:56 +0900

    Fix bug vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -4,12 +4,12 @@ if [ ! -e .vim ]; then
   mkdir .vim
   ln -ns $HOME/dotfiles/.vim $HOME/.vim
 
-  wget https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+  wget https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh -O install.sh
   sh ./install.sh
   rm install.sh
 
   mkdir .vim/colors
-  wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim > .vim/colors/molokai.vim
+  wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O .vim/colors/molokai.vim
 fi
 
 ./link.sh
