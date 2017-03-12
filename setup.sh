fcecaa9a0889679626c887b3140f9eef8bc6fb3e
chuyii <contact@chuyii.net>
2017-03-12 09:16:33 +0900

    Fix bug setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if [ ! -d .vim ]; then
+./link.sh
+
+if [ ! -d .vim/bundle ]; then
   mkdir .vim
   wget -q https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh -O install.sh
   sh ./install.sh
   rm install.sh
+fi
 
+if [ ! -d .vim/colors ]; then
   mkdir .vim/colors
   wget -q https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O .vim/colors/molokai.vim
 fi
-
-./link.sh
