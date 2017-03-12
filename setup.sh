c7f4b4ed2658aed6a756ca32303e11c82c1b62a0
chuyii <contact@chuyii.net>
2017-03-12 11:03:12 +0900

    Add font setup

--- setup.sh
+++ setup.sh
@@ -18,3 +18,13 @@ if [ ! -d $HOME/dotfiles/.vim/colors ]; then
   mkdir $HOME/dotfiles/.vim/colors
   wget -q https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O .vim/colors/molokai.vim
 fi
+
+if [ ! -z $DISPLAY ] && [ `fc-list | grep -c RityDiminished` = 0 ]; then
+  git clone https://github.com/edihbrandon/RictyDiminished.git
+  rm -rf RictyDiminished/{README.md,.git}
+  if [ ! -d $HOME/.fonts ]; then
+    mkdir $HOME/.fonts
+  fi
+  mv RictyDiminished $HOME/.fonts/.
+  fc-cache -fv
+fi
