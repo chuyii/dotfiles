c306e1abd453f6ea87b5d56af15dbd6a9feb284a
chuyii <contact@chuyii.net>
2017-03-12 10:11:38 +0900

    Finalize setup.sh

--- setup.sh
+++ setup.sh
@@ -6,13 +6,15 @@ fi
 
 sh $HOME/dotfiles/link.sh
 
-if [ ! -d $HOME/dotfiles/.vim/bundle ]; then
-  wget -q https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh -O $HOME/dotfiles/install.sh
+cd $HOME/dotfiles
+
+if [ ! -d $HOME/.vim/bundle ]; then
+  wget -q https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh -O install.sh
   sh install.sh
   rm install.sh
 fi
 
 if [ ! -d $HOME/dotfiles/.vim/colors ]; then
   mkdir $HOME/dotfiles/.vim/colors
-  wget -q https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O $HOME/dotfiles/.vim/colors/molokai.vim
+  wget -q https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O .vim/colors/molokai.vim
 fi
