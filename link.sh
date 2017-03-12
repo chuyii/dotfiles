e2a7e345873e73668501e67bc268ab911dcff24c
chuyii <contact@chuyii.net>
2017-03-12 09:57:15 +0900

    Fix link.sh

--- link.sh
+++ link.sh
@@ -2,10 +2,11 @@
 
 for file in $HOME/dotfiles/.??*
 do
+  file=`basename $file`
   [[ "$file" == ".git" ]] && continue
   [[ "$file" == ".gitignore" ]] && continue
   if [ -e $HOME/$file ]; then
     rm -rf $HOME/$file
   fi
-  ln -fns $HOME/dotfiles/file $HOME/$file
+  ln -fns $HOME/dotfiles/$file $HOME/$file
 done
