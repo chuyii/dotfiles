5f4dacd40150124a06090a855eb77ea0074dd880
chuyii <contact@chuyii.net>
2021-04-04 09:07:24 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,13 +20,3 @@ if [ ! -d $HOME/dotfiles/.vim/colors ]; then
   mkdir $HOME/dotfiles/.vim/colors
   wget -q https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O .vim/colors/molokai.vim
 fi
-
-if [ ! -z $DISPLAY ] && [ `fc-list | grep -c RictyDiminished` = 0 ]; then
-  git clone https://github.com/edihbrandon/RictyDiminished.git
-  rm -rf RictyDiminished/{README.md,.git}
-  if [ ! -d $HOME/.fonts ]; then
-    mkdir $HOME/.fonts
-  fi
-  mv RictyDiminished $HOME/.fonts/.
-  fc-cache -fv
-fi
