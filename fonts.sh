a02c200102ca4f739160fe4975ece84b7bee1e9e
chuyii <contact@chuyii.net>
2022-07-04 00:11:32 +0900

    Add fonts

--- fonts.sh
+++ fonts.sh
@@ -10,6 +10,36 @@ cd $TEMP_DIR
 
 isFontUpdated="false"
 
+# for polybar
+if [ ! "$(fc-list "Symbols Nerd Font:style=2048-em")" ]; then
+  cp $SCRIPT_DIR/Symbols-2048-em\ Nerd\ Font\ Complete.ttf ~/.fonts
+  isFontUpdated="true"
+fi
+
+# for polybar
+if [ ! "$(fc-list "Nunito:style=Bold")" ]; then
+  wget -O nunito.zip "https://github.com/googlefonts/nunito/archive/main.zip"
+  unzip nunito.zip
+  mkdir -p ~/.fonts/nunito
+  cp nunito-main/fonts/variable/*.ttf ~/.fonts/nunito
+  isFontUpdated="true"
+fi
+
+# for polybar
+if [ ! "$(fc-list "DejaVu Sans")" ]; then
+  wget -O dejavu.zip "http://sourceforge.net/projects/dejavu/files/dejavu/2.37/dejavu-sans-ttf-2.37.zip"
+  unzip dejavu.zip
+  cp dejavu-sans-ttf-2.37/ttf/DejaVuSans.ttf ~/.fonts
+  isFontUpdated="true"
+fi
+
+# for st
+if [ ! "$(fc-list "Inconsolata:style=Bold")" ]; then
+  cp $SCRIPT_DIR/Inconsolata-*.ttf ~/.fonts
+  isFontUpdated="true"
+fi
+
+# for st
 if [ ! "$(fc-list "NasuM:style=Bold")" ]; then
   wget -O NasuFont.zip https://osdn.net/downloads/users/25/25473/NasuFont-20200227.zip/
   unzip NasuFont.zip
@@ -17,6 +47,7 @@ if [ ! "$(fc-list "NasuM:style=Bold")" ]; then
   isFontUpdated="true"
 fi
 
+# for st
 if [ ! "$(fc-list "Symbols Nerd Font Mono:style=ForConsolas")" ]; then
   cp $SCRIPT_DIR/SymbolsNerdFontMono-ForConsolas.ttf ~/.fonts
   isFontUpdated="true"
